578543a9c685f66b9098ed61cb9d0d7e750ec9ee
basicdev04 <tanishqkandpal.work@gmail.com>
2024-04-25 11:26:13 +0530

    improved authentication

--- backend/middleware.js
+++ backend/middleware.js
@@ -14,7 +14,6 @@ const authMiddleware = (req, res, next) => {
   try {
     const decoded = jwt.verify(token, JWT_SECRET);
     req.userId = decoded.userId;
-
     next();
   } catch (err) {
     return res.status(403).json({});
